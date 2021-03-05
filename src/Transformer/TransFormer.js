7af4194cc3ad0869c59a348aa446212ba98da221
papago89 <papago89@hotmail.com.tw>
2021-03-05 09:32:10 -0800

    chore: reset lint with run lint

--- src/Transformer/TransFormer.js
+++ src/Transformer/TransFormer.js
@@ -1,15 +1,23 @@
+import { Request } from 'httprequest';
+
 class TransFormer {
   constructor() {
   }
-  
+
   urlToKeyword(url) {
 	  return '';
   }
-  
+
   keywordToUrl(keyword) {
 	  return '';
   }
 
+  getHeaderTitle(url) {
+    new Request('POST', '/api/reservations').sendData({ venue_id: 100 }, (err, response) => {
+      if (err) throw err;
+      console.log('got response', res);
+    });
+  }
 }
 
 module.exports = TransFormer;
