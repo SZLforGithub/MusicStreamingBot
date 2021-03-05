c5b5197fc6272a44f07f8d4153a66089a3a2b09b
papago89 <papago89@hotmail.com.tw>
2021-03-05 10:03:33 -0800

    feat init app.js to start bot

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+const logger = require('winston');
+
+
+const PORT = 7777;
+
+
+const client = new Discord.Client({ autoReconnect: true });
+
+logger.level = 'debug';
+
+  logger.info('Connected');
+  logger.info(auth.token);
+client.login(auth.token);
+client.on('ready', function(evt) {
+  logger.info('Connected');
+  logger.info('Logged in as: ');
+  logger.info(client.user.username + ' - (' + client.user.id + ')');
+});
+
+client.on('message', (message) => {
+    let userName = (message.member.nickname == null) ? message.author.username : message.member.nickname;
+    logger.info(`${userName}在${message.channel}說${message.content}`);
+});
