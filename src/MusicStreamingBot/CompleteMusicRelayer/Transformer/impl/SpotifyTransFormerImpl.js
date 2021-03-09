9359553d6934e93e74fca7b5ced044dc1d6ea068
Louis.Su <i0989872540@gmail.com>
2021-03-10 00:05:04 +0800

    fix: spotify empty undefined problem

--- src/MusicStreamingBot/CompleteMusicRelayer/Transformer/impl/SpotifyTransFormerImpl.js
+++ src/MusicStreamingBot/CompleteMusicRelayer/Transformer/impl/SpotifyTransFormerImpl.js
@@ -17,7 +17,12 @@ class SpotifyTransFormerImpl extends TransFormer {
 
         let dataFromSpotifyAPI = JSON.parse(super.getResponse('GET', encodeURI(`https://api.spotify.com/v1/search?type=album&q=${keyword}&decorate_restrictions=false&best_match=true&include_external=audio&limit=10&market=TW`), null, `Bearer ${accessToken}`));
 
-        let bestMatchLink = dataFromSpotifyAPI["best_match"]["items"][0]["tracks"]["items"][0]["external_urls"]["spotify"];
+        let bestMatchLink;
+        try {
+            bestMatchLink = dataFromSpotifyAPI["best_match"]["items"][0]["tracks"]["items"][0]["external_urls"]["spotify"];
+        } catch (error) {
+            bestMatchLink = '';
+        }
 
         return bestMatchLink;
     }
